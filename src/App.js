650d33a8b6d7d5303842587b16a4a792218f878d
alessandrojuza <mascia.alessandro89@gmail.com>
2022-03-22 17:53:26 +0100

    Added search result render

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
+import React, { useState } from "react";
 import "./App.scss";
+import axios from "axios";
 import CocktailResult from "./components/CocktailResult";
-import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 import Logo from "./img/logo.png";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState(""); // User query is stored here and updated on every input change
+  const [cocktailArray, setCocktailArray] = useState([]);
+  const callCocktailApi = () => {
+    axios
+      .get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchQuery}
+    `
+      )
+      .then((res) => {
+        setCocktailArray(res.data.drinks);
+        console.log(res.data.drinks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const searchCocktail = (e) => {
+    setSearchQuery(e.target.value); // Updates searchQuery on every input change
+
+    callCocktailApi();
+  };
+
   return (
     <div className="App">
       <div className="main-container">
         <div className="search-bar-container">
-          <div className="logo-container">
-            <img src={Logo} alt="logo" className="logo" />
-          </div>
+          <img src={Logo} alt="logo" className="logo" />
           <div className="search-bar">
             <input
               type="text"
               className="search-bar-input"
               placeholder="Enter Your Favorite Cocktail:"
+              onChange={searchCocktail}
             />
-            <SearchIcon className="search-icon" />
+            <CloseIcon className="close-icon" />
           </div>
         </div>
         <div className="result-container">
-          <CocktailResult />
+          {/* <CocktailResult /> */}
+          {cocktailArray.map((e) => {
+            return <CocktailResult name={e.strDrink} img={e.strDrinkThumb} />;
+          })}
         </div>
       </div>
     </div>
--- src/components/CocktailResult.js
+++ src/components/CocktailResult.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "../styles/CocktailResult.scss";
 
-const CocktailResult = () => {
-  return <div className="cocktail-result"></div>;
+const CocktailResult = ({ img, name }) => {
+  return (
+    <div className="cocktail-result">
+      <img src={img} alt="cocktail-image" />
+      <p>{name}</p>
+      <div className="expolore-button">
+        <h1>></h1>
+      </div>
+    </div>
+  );
 };
 
 export default CocktailResult;
