08114046a1a788e2ad1a8bfec557cc98e16f6d7c
alessandrojuza <mascia.alessandro89@gmail.com>
2022-03-22 15:14:52 +0100

    Created CocktailResult Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./App.scss";
+import CocktailResult from "./components/CocktailResult";
 import SearchIcon from "@mui/icons-material/Search";
+import Logo from "./img/logo.png";
 
 function App() {
   return (
     <div className="App">
       <div className="main-container">
         <div className="search-bar-container">
-          <div className="logo-container"></div>
+          <div className="logo-container">
+            <img src={Logo} alt="logo" className="logo" />
+          </div>
           <div className="search-bar">
-            <input type="text" className="search-bar-input" />
+            <input
+              type="text"
+              className="search-bar-input"
+              placeholder="Enter Your Favorite Cocktail:"
+            />
             <SearchIcon className="search-icon" />
           </div>
         </div>
-        <div className="result-container"></div>
+        <div className="result-container">
+          <CocktailResult />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/CocktailResult.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/CocktailResult.scss";
+
+const CocktailResult = () => {
+  return <div className="cocktail-result"></div>;
+};
+
+export default CocktailResult;
